d889d597ac0647d29d96466ea6ffd0d80020df35
roman.presn <roman.presn@gmail.com>
2024-11-04 09:33:35 +0100

    small changes to spkpredict

--- src/schnetpack/train/callbacks.py
+++ src/schnetpack/train/callbacks.py
@@ -86,37 +86,37 @@ class PredictionWriter(BasePredictionWriter):
         
         
         d = {}
-        layers = pl_module.model.representation.n_interactions
-        for l in range(layers):
+        #layers = pl_module.model.representation.n_interactions
+        #for l in range(layers):
 
-            d[f"so3krates_layer_{l}"] = {}
-            chi_out = pl_module.model.representation.so3krates_layer[l].record["chi_out"].detach().cpu().numpy()
-            chi_in = pl_module.model.representation.so3krates_layer[l].record["chi_in"].detach().cpu().numpy()
-            feature_out = pl_module.model.representation.so3krates_layer[l].record["features_out"].detach().cpu().numpy()
-            feature_in = pl_module.model.representation.so3krates_layer[l].record["features_in"].detach().cpu().numpy()
-
-
-            d[f"so3krates_layer_{l}"]["chi_in"] = chi_in
-            d[f"so3krates_layer_{l}"]["chi_out"] = chi_out
-            d[f"so3krates_layer_{l}"]["feature_in"] = feature_in
-            d[f"so3krates_layer_{l}"]["feature_out"] = feature_out
+         #   d[f"so3krates_layer_{l}"] = {}
+         #   chi_out = pl_module.model.representation.so3krates_layer[l].record["chi_out"].detach().cpu().numpy()
+         #   chi_in = pl_module.model.representation.so3krates_layer[l].record["chi_in"].detach().cpu().numpy()
+         #   feature_out = pl_module.model.representation.so3krates_layer[l].record["features_out"].detach().cpu().numpy()
+         #   feature_in = pl_module.model.representation.so3krates_layer[l].record["features_in"].detach().cpu().numpy()
 
 
-            geo_att = pl_module.model.representation.so3krates_layer[l].geometry_block.attention_fn.record["alpha"].detach().cpu().numpy()
-            fea_att = pl_module.model.representation.so3krates_layer[l].feature_block.attention_fn.record["alpha"].detach().cpu().numpy()
+         #   d[f"so3krates_layer_{l}"]["chi_in"] = chi_in
+         #   d[f"so3krates_layer_{l}"]["chi_out"] = chi_out
+         #   d[f"so3krates_layer_{l}"]["feature_in"] = feature_in
+         #   d[f"so3krates_layer_{l}"]["feature_out"] = feature_out
+
+
+         #   geo_att = pl_module.model.representation.so3krates_layer[l].geometry_block.attention_fn.record["alpha"].detach().cpu().numpy()
+         #   fea_att = pl_module.model.representation.so3krates_layer[l].feature_block.attention_fn.record["alpha"].detach().cpu().numpy()
             #geo_att = pl_module.model.representation.so3krates_layer[l].geometry_block.attention_fn.record
             #fea_att = pl_module.model.representation.so3krates_layer[0].feature_block.attention_fn.record
 
-            d[f"so3krates_layer_{l}"][f"geometric_head_alpha"] = geo_att
-            d[f"so3krates_layer_{l}"][f"feature_head_alpha"] = fea_att
+         #   d[f"so3krates_layer_{l}"][f"geometric_head_alpha"] = geo_att
+         #   d[f"so3krates_layer_{l}"][f"feature_head_alpha"] = fea_att
 
 
-        prediction.update({"dihedrals":dihedrals})
-        prediction.update({"R":all_R})
-        prediction.update({"Z":all_Z})
-        prediction.update({"so3krates":d})
+        prediction.update({"dihedrals":torch.tensor(dihedrals)})
+        prediction.update({"R":torch.tensor(all_R)})
+        prediction.update({"Z":torch.tensor(all_Z)})
+        #prediction.update({"so3krates":d})
 
-        torch.save(prediction, os.path.join(bdir, f"{batch_idx}.pt"))
+        #torch.save(prediction, os.path.join(bdir, f"{batch_idx}.pt"))
 
     def write_on_epoch_end(
         self,
