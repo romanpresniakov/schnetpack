df4be106c59ef1cc21f24ac24db3ce575d5b4099
roman.presn <roman.presn@gmail.com>
2024-12-11 01:56:23 +0100

    conv

--- src/schnetpack/nn/expansion.py
+++ src/schnetpack/nn/expansion.py
@@ -2,22 +2,29 @@ from typing import Sequence
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+
 from schnetpack.nn import Dense
 
 __all__ = ["SphcBasisExpansion"]
 
 class SphcBasisExpansion(nn.Module):
     
-    def __init__(self, sphc_basis_expansion_fn: nn.Module, sphc_cutoff_fn: nn.Module, n_rbf: int, num_features: Sequence[int]):
+    def __init__(self, sphc_basis_expansion_fn: nn.Module, sphc_cutoff_fn: nn.Module, n_rbf: int):
         super(SphcBasisExpansion, self).__init__()
         self.sphc_basis_expansion_fn = sphc_basis_expansion_fn
         self.sphc_cutoff_fn = sphc_cutoff_fn
-        self.num_features = num_features
-        l1 = Dense(in_features=n_rbf,activation=F.silu,out_features=self.num_features[0])
-        l2 = Dense(in_features=self.num_features[0], activation=F.silu, out_features=self.num_features[1])
-        l3 = Dense(in_features=self.num_features[1],activation=None,out_features=1)
-        self.reduce_fn = nn.Sequential(*[l1,l2,l3])
-        
+        self.reduce_fn = nn.Sequential(
+            nn.Conv1d(in_channels=1, out_channels=16, kernel_size=3, padding=1),
+            nn.SiLU(),
+            nn.AvgPool1d(kernel_size=2),
+            nn.Conv1d(in_channels=16, out_channels=8, kernel_size=3, padding=1),
+            nn.SiLU(),
+            nn.AvgPool1d(kernel_size=2),
+            nn.Flatten(),
+            nn.Linear(8 * (n_rbf // 4), 16),
+            nn.SiLU(),
+            nn.Linear(16, 1)
+        )
         
     def forward(self, inputs: torch.Tensor):
         m_cut_ij = self.sphc_cutoff_fn(inputs)
@@ -28,6 +35,7 @@ class SphcBasisExpansion(nn.Module):
             m_cut_l = m_cut_ij[:, col_idx].unsqueeze(1)
             exp_chi_l = self.sphc_basis_expansion_fn(chi_l) # (n, #rbfs) 
             exp_chi_l = torch.where(m_cut_l != 0, exp_chi_l * m_cut_l, 0) # shape: (n_pairs,n_rbfs)
+            exp_chi_l = exp_chi_l.unsqueeze(1) # 1 in channel
             reduced_chi_l = self.reduce_fn(exp_chi_l)
             output[:, col_idx] = reduced_chi_l.squeeze()
         return output
